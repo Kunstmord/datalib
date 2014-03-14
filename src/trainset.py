21354e0e1f06be96e3ce39512d56fbd79a526f62
georgeoblapenko <kunstmord@gmail.com>
2014-03-14 13:33:37 +0400

    docstrings added

--- src/trainset.py
+++ src/trainset.py
@@ -1,44 +1,23 @@
+"""
+Provides an SQLAlchemy class for a labeled dataset
+"""
 __author__ = 'George Oblapenko'
 __license__ = "GPL"
-__version__ = "0.1"
 __maintainer__ = "George Oblapenko"
 __email__ = "kunstmord@kunstmord.com"
-__status__ = "Development"
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, PickleType, Integer, MetaData
-from sqlalchemy.ext.mutable import Mutable
+from misc import MutableDict
 
 Base = declarative_base()
 metadata = MetaData()
 
 
-class MutableDict(Mutable, dict):
-    @classmethod
-    def coerce(cls, key, value):
-        if not isinstance(value, MutableDict):
-            if isinstance(value, dict):
-                return MutableDict(value)
-            return Mutable.coerce(key, value)
-        else:
-            return value
-
-    def __delitem(self, key):
-        dict.__delitem__(self, key)
-        self.changed()
-
-    def __setitem__(self, key, value):
-        dict.__setitem__(self, key, value)
-        self.changed()
-
-    def __getstate__(self):
-        return dict(self)
-
-    def __setstate__(self, state):
-        self.update(state)
-
-
 class TrainSet(Base):
+    """
+    SQLAlchemy class for a labeled dataset
+    """
     __tablename__ = 'train set'
     id = Column(Integer, primary_key=True)
     real_id = Column(String(60))
